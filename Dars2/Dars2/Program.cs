929340a10501778c5cd58adfc44966293ec67c9c
Nodir979 <sakkizincigla@gmail.com>
2025-12-02 18:03:04 +0500

    Dars2

---
+++ Dars2/Dars2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Dars2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello world");
+        }
+    }
+}
