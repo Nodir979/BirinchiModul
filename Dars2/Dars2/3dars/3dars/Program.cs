c452352fa161b4d84acd34d34f60d325f67bee1a
Nodir979 <sakkizincigla@gmail.com>
2025-12-06 15:01:42 +0500

    3dars

---
+++ Dars2/Dars2/3dars/3dars/Program.cs
@@ -0,0 +1,398 @@
+﻿namespace _3dars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //for (int i = 1; i <= 20; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //2
+            //for (int i = 25; i <=100; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //3
+            //for (int i = 12; i <=200; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //4
+            //for (int i = 15; i >= 2; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //5
+            //for (int i = 120; i >= 23; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //6
+            //for (int i = 10; i <= 50;  i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //7
+            //for (int i = 120; i <= 250;  i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //8
+            //for (int i = 100; i >= 50; i--)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //9
+            //for (int i =80; i >= 20; i--)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //10
+            //int a = 34;
+            //int b = 60;
+
+            //for (int i = a; i <= b; i++)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //11
+            //Console.Write("a = ");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.Write("b = ");
+            //int b = int.Parse(Console.ReadLine());  
+
+            //for (int i = a; i <= b; i++)
+            //{
+            //    if(i % 4 == 0 && i % 7 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //12
+            //Console.Write("a = ");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.Write("b = ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //int count = 0;
+
+            //for (int i = a; i <= b; i++)
+            //{
+            //    if(i % 8 == 0)
+            //    {
+            //      count++;
+            //    }
+            //}
+            //Console.WriteLine( + count);
+
+            //13
+            //Console.Write("a = ");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.Write("b = ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //for (int i = b; i >= a; i--)
+            //{
+            //    if(i % 9 == 0 || i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //14
+            //int sum = 0;
+
+            //for(int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        sum += i;
+            //    }
+            //}
+            //Console.WriteLine( + sum);
+
+            //2.1
+            //for (int i = 100; i <= 999;  i++)
+            //{
+            //    int sumOfDigits = (i / 100) + ((i / 10) % 10) + (i % 10);
+
+            //    if (sumOfDigits > 20)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //2.2
+            //for (int i = 100; i <= 999; i++)
+            //{
+            //    int yuz = i / 100;
+            //    int birlik = i % 10;
+
+            //    if(yuz == birlik)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //2.3
+            //int count = 0;
+            //int sum = 0;
+
+            //for (int i = 10; i <= 99; i++)
+            //{
+            //    int tens = i / 10;
+            //    int units = i % 10;
+
+            //    int product = tens * units;
+
+            //    if (product > 12)
+            //    {
+            //        count++;
+            //        sum += i;
+            //    }
+            //}
+            //Console.WriteLine(+ count);
+            //Console.WriteLine(+ sum);
+
+            //2.4
+            //for (int i = 100; i <= 999; i++) 
+            //{
+            //    int tens = (i / 10) % 10;      
+
+            //    if (tens == 2 || tens == 5)    
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //2.5
+            //Console.Write("a sonini kiriting: ");
+            //int a = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= a; i++)
+            //{
+            //    Console.WriteLine(a);
+            //}
+
+            //2.6
+            //Console.Write("a sonini kiriting: ");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.Write("b sonini kiriting: ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= b; i++)
+            //{
+            //    Console.WriteLine(a);
+            //}
+
+            //2.7
+            //Console.Write("b sonini kiriting: ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //Console.Write("a sonini kiriting: ");
+            //int a = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= a; i++)
+            //{
+            //    Console.WriteLine(b);
+            //}
+
+            //2.8
+            //Console.Write("a sonini kiriting: ");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(a + " sonining bo'luvchilari:");
+
+            //for (int i = 1; i <= a; i++)
+            //{
+            //    if (a % i == 0)  
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //2.9
+            //Console.Write("a sonini kiriting: ");
+            //int a = int.Parse(Console.ReadLine());
+
+            //int count = 0;
+
+            //for (int i = 1; i <= a; i++)
+            //{
+            //    if (a % i == 0) 
+            //    {
+            //        count++;
+            //    }
+            //}
+
+            //Console.WriteLine(a + " sonining bo'luvchilari miqdori: " + count);
+
+            //2.10
+            //Console.Write("a sonini kiriting: ");
+            //int a = int.Parse(Console.ReadLine());
+
+            //int sum = 0;
+
+            //for (int i = 1; i <= a; i++)
+            //{
+            //    if (a % i == 0) 
+            //    {
+            //        sum += i;    
+            //    }
+            //}
+
+            //Console.WriteLine(a + " sonining bo'luvchilar yig'indisi: " + sum);
+
+            //1
+            //for (int i = 14; i <= 20; i++)
+            //{
+            //    Console.Write(i + " ");
+            //}
+            //Console.WriteLine();
+
+            //2
+            //for (int i = 11; i <= 99; i++)
+            //{
+            //    Console.Write(i + " ");
+            //}
+            //Console.WriteLine();
+
+            //3
+            //for (int i = 12; i <= 200; i++)
+            //{
+            //    Console.Write(i + " ");
+            //}
+            //Console.WriteLine();
+
+            //4
+            //for (int i = 25; i >= 10; i--)
+            //{
+            //    Console.Write(i + " ");
+            //}
+            //Console.WriteLine();
+
+            //5
+            //for (int i = 500; i >= 80; i--)
+            //{
+            //    Console.Write(i + " ");
+            //}
+            //Console.WriteLine();
+
+            //6
+            //for (int i = 11; i <= 88; i++)
+            //{
+            //    if (i % 2 != 0)
+            //        Console.Write(i + " ");
+            //}
+            //Console.WriteLine();
+
+            //7
+            //for (int i = 12; i <= 25; i++)
+            //{
+            //    if (i % 2 == 0)
+            //        Console.Write(i + " ");
+            //}
+            //Console.WriteLine();
+
+            //8
+            //for (int i = 15; i <= 88; i++)
+            //{
+            //    if (i % 2 != 0)
+            //        Console.Write(i + " ");
+            //}
+            //Console.WriteLine();
+
+            //9
+            //for (int i = 99; i >= 10; i--)
+            //{
+            //    if (i % 2 == 0)
+            //        Console.Write(i + " ");
+            //}
+            //Console.WriteLine();
+
+            //10
+            //int a = 10;
+            //int b = 30;
+            //for (int i = a; i <= b; i++)
+            //{
+            //    if (i % 3 == 0)
+            //        Console.Write(i + " ");
+            //}
+            //Console.WriteLine();
+
+            //11
+            //a = 10;
+            //b = 60;
+            //for (int i = a; i <= b; i++)
+            //{
+            //    if (i % 5 == 0 && i % 6 == 0)
+            //        Console.Write(i + " ");
+            //}
+            //Console.WriteLine();
+
+            //12
+            //a = 10;
+            //b = 100;
+            //int count = 0;
+            //for (int i = a; i <= b; i++)
+            //{
+            //    if (i % 10 == 0)
+            //        count++;
+            //}
+            //Console.WriteLine("10 ga karrali sonlar soni: " + count);
+
+            //13
+            //a = 10;
+            //b = 50;
+            //for (int i = b; i >= a; i--)
+            //{
+            //    if (i % 7 == 0 || i % 8 == 0)
+            //        Console.Write(i + " ");
+            //}
+            //Console.WriteLine();
+
+            //14
+            //int sumEven = 0;
+            //for (int i = 2; i <= 100; i += 2)
+            //{
+            //    sumEven += i;
+            //}
+            //Console.WriteLine("1 dan 100 gacha juft sonlar yig'indisi: " + sumEven);
+
+            
+
+        }
+    }
+}
